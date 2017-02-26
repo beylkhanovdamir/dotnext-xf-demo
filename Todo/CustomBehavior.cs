07250346989703da8a73bf08e91961405b150be0
Damir Beylkhanov <damir.beylkhanov@muranosoft.com>
2017-02-26 23:19:18 +0300

    add custom behavior for check on max length

---
+++ Todo/CustomBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public class CustomBehavior : Behavior<Entry>
+	{
+		protected override void OnAttachedTo(Entry entry)
+		{
+			entry.TextChanged += OnEntryTextChanged;
+			base.OnAttachedTo(entry);
+		}
+
+		protected override void OnDetachingFrom(Entry entry)
+		{
+			entry.TextChanged -= OnEntryTextChanged;
+			base.OnDetachingFrom(entry);
+		}
+
+		void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+		{
+			bool isValid = args.NewTextValue.Length < 15;
+			((Entry)sender).Text = isValid ? args.NewTextValue : args.OldTextValue;
+		}
+	}
+}
