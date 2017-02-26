db4856c83850db0de0fd61af666d9b18f92540ec
Damir Beylkhanov <damir.beylkhanov@muranosoft.com>
2017-02-26 22:58:22 +0300

    add custom renderer for ios

---
+++ Todo.iOS/CustomEntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo;
+using Todo.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+
+namespace Todo.iOS
+{
+	public class CustomEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				Control.SpellCheckingType = UITextSpellCheckingType.No;             // No Spellchecking
+				Control.AutocorrectionType = UITextAutocorrectionType.No;           // No Autocorrection
+				Control.AutocapitalizationType = UITextAutocapitalizationType.None; // No Autocapitalization
+				Control.ReturnKeyType = UIReturnKeyType.Go;
+			}
+		}
+	}
+}
