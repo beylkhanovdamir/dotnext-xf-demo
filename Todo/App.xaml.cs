4d9203d467d737b9a430b129226b95a2bdfbcab1
Damir Beylkhanov <damir.beylkhanov@muranosoft.com>
2017-04-22 20:36:08 +0300

    added integration with Mobile Center

--- Todo/App.xaml.cs
+++ Todo/App.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 using Todo.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -18,6 +21,10 @@ namespace Todo
 		public App()
 		{
 			InitializeComponent();
+
+			MobileCenter.Start("ios=37ef0777-8c25-459e-95e9-19a9e5ae5220;" +
+				   "android=e04e4274-9cd7-44fb-a984-70558d46eb82",
+				   typeof(Analytics), typeof(Crashes));
 		}
 
 		protected override void OnStart()
@@ -30,7 +37,6 @@ namespace Todo
 			MainPage = nav;
 
 		}
-
 		public static TodoItemDatabase Database
 		{
 			get
