44c42e3f0619611ba937f90994abd240aaa02112
Damir Beylkhanov <damir.beylkhanov@muranosoft.com>
2017-04-23 17:19:59 +0300

    add trigger
    add effect

---
+++ Todo/ShadowEffect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public class ShadowEffect : RoutingEffect
+	{
+		public float Radius { get; set; }
+
+		public Color Color { get; set; }
+
+		public float DistanceX { get; set; }
+
+		public float DistanceY { get; set; }
+
+		public ShadowEffect() : base("Todo.ButtonShadowEffect")
+		{
+		}
+	}
+}
